65c0e89dd667999cbcbe7cde335dcd62b9637691
Salman Naseem <salman304936@email.com>
2026-05-15 19:49:48 +0500

    feat : store UI , store manager class with just function calling

--- Scenes/main.gd
+++ Scenes/main.gd
@@ -90,3 +90,7 @@ func game_logic_reset():
 	coin_spawner.clear_coins()
 	current_score = 0
 	ui_manager.changeScore(0)
+
+
+func _on_10750_coin_pressed() -> void:
+	pass # Replace with function body.
